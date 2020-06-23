9d770c8fc9991b695232aa775e9383ab8b336396
schuj <Justin.Schuller@pega.com>
2020-06-23 16:57:46 -0400

    added PropTypes as type checker for components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes, { object } from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -284,6 +285,13 @@ const Search = ({
             </button>
         </form>
 
+Search.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+}        
+
 const Button = ({onClick, className, children}) =>
         <button
             onClick={onClick}
@@ -291,7 +299,25 @@ const Button = ({onClick, className, children}) =>
             type="button"
         >
             {children}
-        </button>        
+        </button>
+
+// In ES6 className could be defined in the component signature with a default parameter.
+// However another way is to use default prop to ensure the property is set to a default value when
+// the parent component does not specify it.
+Button.defaultProps = {
+    className: '',
+};        
+
+// React comes with a built in type checker to prevent bugs. You can use PropTypes to
+// describe your component interface. All the props passed from a parent compnent to a child
+// get validated based on the PropTypes interface assigned to the child.
+Button.propTypes = {
+    // Using isRquired makes the property required, otherwise it can be null or undefined
+    onClick: PropTypes.func.isRequired,
+    // className is nt required because it can default to an empty string
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired,
+}        
 
 
 const largeColumn = {
@@ -326,6 +352,27 @@ const Table = ({list, onDismiss}) =>
                 </div>
             )}
         </div>
+
+/*
+Table.propTypes = {
+    list: PropTypes.array.isRequired,
+    onDismiss: PropTypes.func.isRequired,
+} 
+*/
+
+// While the above works, you can define the content of an array PropType more explicity
+Table.propTypes = {
+    list: PropTypes.arrayOf(
+        PropTypes.shape({
+            objectID: PropTypes.string.isRequired,
+            author: PropTypes.string,
+            url: PropTypes.string,
+            num_comments: PropTypes.number,
+            points: PropTypes.number,
+        })
+    ).isRequired,
+    onDismiss: PropTypes.func.isRequired,
+}        
 
 export default App;
 
