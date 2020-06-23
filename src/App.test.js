962aeda665cd831ebe691689d03bb787c07dc97c
schuj <Justin.Schuller@pega.com>
2020-06-23 16:05:34 -0400

    Added Enzyme unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+
+// Enzyme is a testing utility by Aitbnb to assert, manipulate, and traverse React components.
+// It is used to conduct unit tests to complement snapshot tests in React. These do not come
+// bundled with create-react-app
+import Enzyme, { shallow } from 'enzyme';
+import Adaptor from 'enzyme-adapter-react-16';
+
 import App, { Search, Button, Table } from './App';
+
+Enzyme.configure({ adapter: new Adaptor() });
 
 // run npm test from HackerNew to execute test
 
@@ -56,7 +65,15 @@ describe('Search', () => {
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
       });
-    
+
+      it('shows two items in the list', () => {
+        // Shallow renders the compnent without its child components, so you can dedicate the test to one component
+        const element = shallow(
+          <Button>Click me!</Button>
+        );
+
+        expect(element.text().includes("Click me!")).toBe(true);
+      });      
     });
 
     describe('Table', () => {
@@ -80,6 +97,20 @@ describe('Search', () => {
           );
           const tree = component.toJSON();
           expect(tree).toMatchSnapshot();
+        });
+
+        // Enzyme has three rendering mechanism in its API. shallow() as shown above, and also mount() and render()
+        //  - Always begin with a shallow test
+        //  - If componentDidMount() or componentDidUpdate() should be tested, use moount()
+        //  - If you want to test a component's children rendering with less overhead than mount() and you
+        //    are not interested in lifecycle methods, use render()
+        it('shows two items in the list', () => {
+          // shallow renders the compnent without its child components, so you can dedicate the test to one component
+          const element = shallow(
+            <Table { ...props }/>
+          );
+
+          expect(element.find('.table-row').length).toBe(2);
         });
       
       });    
