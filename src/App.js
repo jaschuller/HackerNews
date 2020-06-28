34935be639424095104c3a14a63124cadac3367a
schuj <Justin.Schuller@pega.com>
2020-06-28 10:43:03 -0400

    added example of using Ref to access a DOM element for purposed of focusing search input on application render

--- src/App.js
+++ src/App.js
@@ -254,6 +254,50 @@ class App extends Component {
     }
 }
 
+// ES6 Class Component. The this object of an ES6 class component helps us to reference the DOM element with the ref attribute.
+// The official documentation mentions three cases where you need to access the DOM node (this us usually an anti pattern in React,
+// because you should use its declarative way of doing things and its unidirectional data flow).
+//  - to use the DOM API (focus, media playback, etc.)
+//  - to invoke imperative DOM node animations
+//  - to integrate with a third-party library that nees the DOM node (e.g. D3.js)
+class Search extends Component {
+    // Now you can focus the input field when the component mounted by using the this object, 
+    // the appropriate lifecycle method, and the DOM API. The input should be focused when the application renders.
+    componentDidMount() {
+        if (this.input) {
+            this.input.focus();
+        }
+    }
+    render() {
+        const {
+            value,
+            onChange,
+            onSubmit,
+            children
+        } = this.props;
+        return (
+            <form onSubmit={onSubmit}>
+                <input
+                type="text"
+                value={value}
+                onChange={onChange}
+                ref={el => this.input = el}
+                />
+                <button type="submit">
+                    {children}
+                </button>
+            </form>
+        );
+    }
+}
+
+Search.propTypes = {
+value: PropTypes.string,
+onChange: PropTypes.func.isRequired,
+onSubmit: PropTypes.func.isRequired,
+children: PropTypes.node.isRequired,
+}
+
 // Composable Components: by passing children prop ("Filter by" used here) the Search 
 // component can destructure the children property from the props object and specify
 // where it should be displayed (next to the input in this scenario)
@@ -268,7 +312,7 @@ class App extends Component {
 //
 // if we needed to do more with state, we could refactor this into an ES6 functional component
 // Use destructuring in the function signature as best practice
-const Search = ({
+const SearchFunctionalStateless = ({
         value, 
         onChange,
         onSubmit, 
